5e1c198eff372edda84adf4216f4c9c228043230
unknown <danrlei_vieira@hotmail.com>
2025-02-17 22:21:31 -0300

    refactor: API key in .env, favicon index.html, removed the path /cinemania/ as a base in vite.config.js and improved the styling in NavBar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
+import { Outlet } from "react-router-dom";
 
-import { Outlet } from 'react-router-dom'
-
-import NavBar from './components/NavBar'
-
-import './App.css'
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <NavBar />
       <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 import "./Navbar.css";
+import { TbMovie } from "react-icons/tb";
 
 const Navbar = () => {
   const [search, setSearch] = useState("");
@@ -19,22 +20,24 @@ const Navbar = () => {
 
   return (
     <nav id="navbar">
-      <h2>
-        <Link to="/">
-          <BiCameraMovie /> MoviesLib
-        </Link>
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Busque um filme"
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
-        />
-        <button type="submit">
-          <BiSearchAlt2 />
-        </button>
-      </form>
+      <div className="container">
+        <h2>
+          <Link to="/">
+            <TbMovie /> Cine Mania
+          </Link>
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Pesquise um filme"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <button type="submit">
+            <BiSearchAlt2 />
+          </button>
+        </form>
+      </div>
     </nav>
   );
 };
